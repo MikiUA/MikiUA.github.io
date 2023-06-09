64e9c267173971ceeb8b0388a1f1e123deb68f02
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-09 15:44:46 +0300

    updated ukrainian language

--- script/language text.js
+++ script/language text.js
@@ -1,98 +1,193 @@
-const EN={
-    "position":"Web (fullstack) Developer",
-    "name":"Maksym Melnykov",
-    "description":"Young guy willing to work, ready to bring exciting ideas into life, to improve and create new experience for the customers",
-    "about myself label":"About me",
-    "about myself":"I am interested in web development technologies, I want to create and share projects close to the user. Am sure that by working in this field I can improve my knowledge and hone my skills and then with the team help making peoples lives easier.",
-    "downloadable_resume label":"download resume",
-    "downloadable_resume link":"#.html",
-    "projects label":"Project experience",
-    "projects":[{
-        "title":"Virtual instruments website",
-        "tech_stack":["Javascript","CSS"],
-        "role":"Developer",
-        "link":"https://iridescent-bombolone-954cbb.netlify.app/",
-        "codelink":"https://github.com/MikiUA/InstrumentsHub",
-        "responsibilities":["Simplify and unify javascript code from different implementations","Make a comfortable rubber design"]
-    },{
-        "title":"Phonebook",
-        "link":"https://mikiua.github.io/goit-react-hw/hw-08-phonebook/",
-        "codelink":"https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
-        "tech_stack":["React","Redux","Material UI"],
-        "role":"React Student",
-        "responsibilities":["Develop a simple application using wide variety of technology","Implement login system","Make use of redux toolkit","Simlify and beautify design using an open-source library"]
-    },{
-        "title":"Movie Gallery",
-        "role":"React Student",
-        "tech_stack":["React"],
-        "link":"https://mikiua.github.io/goit-react-hw/hw-05-movies/",
-        "codelink":"https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
-        "responsibilities":["Develop a user interface to work with themoviedb free api"],
-    },{
-        "title":"Chat website",
-        "tech_stack":["Node.js","MongoDB"],
-        "codelink":"https://github.com/MikiUA/Chat-Website/tree/main/screenshots",
-        "role":"Developer",
-        "responsibilities":["Learn the basics of Node.js and web development in general","Implement a working website of high enough quality to be considered usable."],
-        "screenshot":""
+const EN = {
+    "position": "Web (fullstack) Developer",
+    "name": "Maksym Melnykov",
+    "description": "Young guy willing to work, ready to bring exciting ideas into life, to improve and create new experience for the customers",
+    "about myself label": "About me",
+    "about myself": "I am interested in web development technologies, I want to create and share projects close to the user. Am sure that by working in this field I can improve my knowledge and hone my skills and then with the team help making peoples lives easier.",
+    "downloadable_resume label": "download resume",
+    "downloadable_resume link": "#.html",
+    "projects label": "Project experience",
+    "projects": [{
+        "title": "Virtual instruments website",
+        "tech_stack": ["Javascript", "CSS"],
+        "role": "Developer",
+        "link": "https://iridescent-bombolone-954cbb.netlify.app/",
+        "codelink": "https://github.com/MikiUA/InstrumentsHub",
+        "responsibilities": ["Simplify and unify javascript code from different implementations", "Make a comfortable rubber design"]
+    }, {
+        "title": "Phonebook",
+        "link": "https://mikiua.github.io/goit-react-hw/hw-08-phonebook/",
+        "codelink": "https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
+        "tech_stack": ["React", "Redux", "Material UI"],
+        "role": "React Student",
+        "responsibilities": ["Develop a simple application using wide variety of technology", "Implement login system", "Make use of redux toolkit", "Simlify and beautify design using an open-source library"]
+    }, {
+        "title": "Movie Gallery",
+        "role": "React Student",
+        "tech_stack": ["React"],
+        "link": "https://mikiua.github.io/goit-react-hw/hw-05-movies/",
+        "codelink": "https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
+        "responsibilities": ["Develop a user interface to work with themoviedb free api"],
+    }, {
+        "title": "Chat website",
+        "tech_stack": ["Node.js", "MongoDB"],
+        "codelink": "https://github.com/MikiUA/Chat-Website/tree/main/screenshots",
+        "role": "Developer",
+        "responsibilities": ["Learn the basics of Node.js and web development in general", "Implement a working website of high enough quality to be considered usable."],
+        "screenshot": ""
     },
-    ,{
-        "title":"My resume",
-        "tech_stack":["HTML","CSS"],
-        "link":"#.html",
-        "screenshot":""
-    // },{
-    //     "title":"...more can be seen here",
-    //     "tech_stack":["C++"],
-    //     "link":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    //     "screenshot":""
+        , {
+        "title": "My resume",
+        "tech_stack": ["HTML", "CSS"],
+        "link": "#.html",
+        "screenshot": ""
+    }, {
+        "title": "...more can be seen here",
+        "tech_stack": ["C++"],
+        "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "screenshot": ""
     }],
-    "work exp label":"Work experience",
-    "work exp":[{
-        "location":"OneReach.ai",
-        "location_href":"",
-        "direction":"Javascript low-code developer",
-        "direction_href":"",
-        "time_period":"September 2022",
-        "responsibility_list":["Develop block-scheme task solutions","Find and report bugs for in-development low-code platform"]
-    },{
-        "location":'Supermarket "Class"',
-        "direction":"Handyman",
-        "time_period":"April-May 2018",
-        "responsibility_list":[]
+    "work exp label": "Work experience",
+    "work exp": [{
+        "location": "OneReach.ai",
+        "location_href": "",
+        "direction": "Javascript low-code developer",
+        "direction_href": "",
+        "time_period": "September 2022",
+        "responsibility_list": ["Develop block-scheme task solutions", "Find and report bugs for in-development low-code platform"]
+    }, {
+        "location": 'Supermarket "Class"',
+        "direction": "Handyman",
+        "time_period": "April-May 2018",
+        "responsibility_list": []
     },],
-    "education label":"Education",
-    "education":[
+    "education label": "Education",
+    "education": [
         {
-            "location":"National Technical University<br>«Kharkiv Polytechnic Institute»",
-            "location_href":"https://www.kpi.kharkov.ua/eng/",
-            "direction":"<a href='http://web.kpi.kharkov.ua/apm/golovna/'>Mathematics and statistics</a> | <a href='img/diploma.jpg'>Diploma</a>",
-            "direction_href":"",
-            "time_period":"September 2016 - June 2021 | Ukraine, Kharkiv",
-            "diploma":""
+            "location": "National Technical University<br>«Kharkiv Polytechnic Institute»",
+            "location_href": "https://www.kpi.kharkov.ua/eng/",
+            "direction": "<a href='http://web.kpi.kharkov.ua/apm/golovna/'>Mathematics and statistics</a> | <a href='img/diploma.jpg'>Diploma</a>",
+            "direction_href": "",
+            "time_period": "September 2016 - June 2021 | Ukraine, Kharkiv",
+            "diploma": ""
 
-    },{
-        "location":"IT School GoIT",
-        "location_href":"",
-        "direction":"Web developement crash course",
-        "direction_href":"",
-        "time_period":"May 2022 - June 2022"
+        }, {
+            "location": "IT School GoIT",
+            "location_href": "",
+            "direction": "Web developement crash course",
+            "direction_href": "",
+            "time_period": "May 2022 - June 2022"
 
-},],
-    "contacts label":"Contacts",
-    "contacts":[
+        },],
+    "contacts label": "Contacts",
+    "contacts": [
         {
-            "label":"",
-            "href":""
+            "label": "",
+            "href": ""
         }
     ],
-    "tech skills label":"Tech Skills",
-    "tech skills":["C++","Javascript","HTML, CSS","React","Node.js","MongoDB, SQL","Figma"],
-    "more tech skills":["English level B2","Microsoft Word, Excel","Mathematics, statistics", "Drivers licence (B)"],
-    "soft skills label":"Soft Skills",
-    "soft skills":["Problem solving","Creative","Responsible","Preparative","Incorporated AI into work cycle"],
-    "lang skills label":"Languages",
-    "lang skills":["English level <a href='img/StatementOfResult.pdf'>B2</a>","Ukrainian Native","Russian Native"],
+    "tech skills label": "Tech Skills",
+    "tech skills": ["C++", "Javascript", "HTML, CSS", "React", "Node.js", "MongoDB, SQL", "Figma"],
+    "more tech skills": ["English level B2", "Microsoft Word, Excel", "Mathematics, statistics", "Drivers licence (B)"],
+    "soft skills label": "Soft Skills",
+    "soft skills": ["Problem solving", "Creative", "Responsible", "Preparative", "Incorporated AI into work cycle"],
+    "lang skills label": "Languages",
+    "lang skills": ["English level <a href='img/StatementOfResult.pdf'>B2</a>", "Ukrainian Native", "Russian Native"],
 }
 
-const UA={};
+const UA = {
+    "position": "Веб розробник (фуллстак)",
+    "name": "Максим Мельников",
+    "description": "",
+    "about myself label": "Про мене",
+    "about myself": "",
+    "downloadable_resume label": "завантажити резюме",
+    "downloadable_resume link": "#.html",
+    "projects label": "Проектний досвід",
+    "projects": [{
+        "title": "Віртуальне піаніно",
+        "tech_stack": ["Javascript", "CSS"],
+        "role": "Творчий проект",
+        "link": "https://iridescent-bombolone-954cbb.netlify.app/",
+        "codelink": "https://github.com/MikiUA/InstrumentsHub",
+        "responsibilities": ["Simplify and unify javascript code from different implementations", "Make a comfortable rubber design"]
+    }, {
+        "title": "Книга контактів",
+        "link": "https://mikiua.github.io/goit-react-hw/hw-08-phonebook/",
+        "codelink": "https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
+        "tech_stack": ["React", "Redux", "Material UI"],
+        "role": "Навчальний проект",
+        "responsibilities": ["Develop a simple application using wide variety of technology", "Implement login system", "Make use of redux toolkit", "Simlify and beautify design using an open-source library"]
+    }, {
+        "title": "Галерея фільмів",
+        "role": "React Student",
+        "tech_stack": ["React"],
+        "link": "https://mikiua.github.io/goit-react-hw/hw-05-movies/",
+        "codelink": "https://github.com/MikiUA/goit-react-hw/tree/main/src/hw-05-movies",
+        "responsibilities": ["Develop a user interface to work with themoviedb free api"],
+    }, {
+        "title": "Chat website",
+        "tech_stack": ["Node.js", "MongoDB"],
+        "codelink": "https://github.com/MikiUA/Chat-Website/tree/main/screenshots",
+        "role": "Developer",
+        "responsibilities": ["Learn the basics of Node.js and web development in general", "Implement a working website of high enough quality to be considered usable."],
+        "screenshot": ""
+    },
+        , {
+        "title": "Моє резюме",
+        "tech_stack": ["HTML", "CSS"],
+        "link": "#.html",
+        "screenshot": ""
+    }, {
+        "title": "Більше на гітхабі",
+        "tech_stack": ["C++"],
+        "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "screenshot": ""
+    }],
+    "work exp label": "Досвід роботи",
+    "work exp": [{
+        "location": "OneReach.ai",
+        "location_href": "",
+        "direction": "Javascript low-code developer",
+        "direction_href": "",
+        "time_period": "September 2022",
+        "responsibility_list": ["Develop block-scheme task solutions", "Find and report bugs for in-development low-code platform"]
+    }, {
+        "location": 'Supermarket "Class"',
+        "direction": "Handyman",
+        "time_period": "April-May 2018",
+        "responsibility_list": []
+    },],
+    "education label": "Освіта",
+    "education": [
+        {
+            "location": "Національний Технічний Університет<br>«Харківський Політехнічний Інститут»",
+            "location_href": "https://www.kpi.kharkov.ua/eng/",
+            "direction": "<a href='http://web.kpi.kharkov.ua/apm/golovna/'>Математика та статистика</a> | <a href='img/diploma.jpg'>посилання на диплом</a>",
+            "direction_href": "",
+            "time_period": "September 2016 - June 2021 | Ukraine, Kharkiv",
+            "diploma": ""
+
+        }, {
+            "location": "IT Школа GoIT",
+            "location_href": "",
+            "direction": "Курс фуллстак розробника",
+            "direction_href": "",
+            "time_period": "May 2022 - June 2023"
+
+        },],
+    "contacts label": "Контакти",
+    "contacts": [
+        {
+            "label": "",
+            "href": ""
+        }
+    ],
+    "tech skills label": "Технічні навички",
+    "tech skills": ["C++", "Javascript", "HTML, CSS", "React", "Node.js", "MongoDB, SQL", "Figma"],
+    "more tech skills": ["Англійска рівня B2", "Microsoft Word, Excel", "Математика та статистика", "Водійскі права (B)"],
+    "soft skills label": "Софт скіли",
+    "soft skills": ["Вирішення задач", "Креативність", "Відповідальність", "Використовую AI для ефективнішої роботи"],
+    "lang skills label": "Мови",
+    "lang skills": ["Англійска <a href='img/StatementOfResult.pdf'>рівня B2</a>", "Українська (рідна)", "Російска (вільне володіння)"],
+};
