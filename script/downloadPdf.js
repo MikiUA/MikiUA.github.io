ba45a9278ce804b5088405e41b1d991db239dfb5
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-26 18:14:51 +0300

    remove selector from downloadable

--- script/downloadPdf.js
+++ script/downloadPdf.js
@@ -1,5 +1,5 @@
 // Function to download the current web page as a PDF
-function downloadAsPdf() {
+async function downloadAsPdf() {
   const options = {
     filename: 'downloaded_page.pdf',
     image: { type: 'jpeg', quality: 0.98 },
@@ -12,20 +12,27 @@ function downloadAsPdf() {
 
   // Generate the PDF
   html2pdf().set(options).from(content).outputPdf('datauristring')
-    .then(function(pdfDataUri) {
+    .then(function (pdfDataUri) {
       // Create a temporary <a> element and initiate the download
       const a = document.createElement('a');
       a.href = pdfDataUri;
       a.download = options.filename;
       a.click();
+      return
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error('Error generating PDF:', error);
+      return
     });
 }
 // Add a download button or trigger the download on page load
 // Replace "downloadButton" with the ID or class of your download button element
 document.addEventListener('DOMContentLoaded', function () {
+  async function download() {
+    document.getElementById('topleft').style.display = 'none';
+    await downloadAsPdf();
+    document.getElementById('topleft').style.display = 'block';
+  }
   const downloadButton = document.getElementById('downloadable_resume');
-  downloadButton.addEventListener('click', downloadAsPdf);
+  downloadButton.addEventListener('click', download);
 });
